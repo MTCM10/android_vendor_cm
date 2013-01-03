84ede007d43e99ba30d6e2791f7ca1689b3d3633
Gannon Macdonald <gannon050197@gmail.com>
2013-01-02 19:52:44 -0600

    Cleaning up space
    Change-Id: I97bfa23f98f7e44ea3c9041959379c1fb14b41e4

--- config/common_full.mk
+++ config/common_full.mk
@@ -5,21 +5,16 @@ $(call inherit-product, vendor/cm/config/common.mk)
 include frameworks/base/data/sounds/NewAudio.mk
 
 # Extra Ringtones
-include frameworks/base/data/sounds/AudioPackageNewWave.mk
+#include frameworks/base/data/sounds/AudioPackageNewWave.mk
 
 # Bring in all video files
-$(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
+#$(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
 
 # Include CM audio files
 include vendor/cm/config/cm_audio.mk
 
 # Optional CM packages
 PRODUCT_PACKAGES += \
-    HoloSpiralWallpaper \
-    MagicSmokeWallpapers \
-    NoiseField \
-    Galaxy4 \
     LiveWallpapers \
     LiveWallpapersPicker \
-    VisualizationWallpapers \
-    PhaseBeam
+    VisualizationWallpapers
